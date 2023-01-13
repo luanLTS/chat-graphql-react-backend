e1823f70fd13e12e9ed43f1b258d380b06ddedef
Luan Teixeira <luanlts@outlook.com>
2023-01-13 20:41:16 -0300

    feat: fim da apostila 02 servidor graphql implementado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log("Hello, GraphQL");
 
-import { createSchema, createYoga } from "graphql-yoga";
+import { createSchema, createYoga, createPubSub } from "graphql-yoga";
 import { createServer } from "node:http";
 
 // ? base de dados
@@ -10,6 +10,8 @@ import { participacoes } from "./database/holdings";
 import { mensagens } from "./database/messages";
 
 // schema -> objeto que agrupa as funcionalidades que existem no servidor, interfaces e resolvers
+
+const pubSub = createPubSub();
 
 const schema = createSchema({
     // define o que existe na interface(query, mutation, subcription)
@@ -28,6 +30,7 @@ const schema = createSchema({
             texto: String!
             usuario: Usuario!   # cada mensagem pode ter o objeto usuario inteiro
             ambiente: Ambiente!
+            data: String!
         }
         type Ambiente {
             id: ID!
@@ -46,7 +49,18 @@ const schema = createSchema({
             hello: String!  # "!"" fala que é obrigacao devolver String, caso não tenha ! pode ser enviado outro tipo
             ambientes: [Ambiente!]!
             existe(nome: String!, senha: String!): Usuario   #(parametro: Tipo)
-            usuarios: [Usuario!]!
+            usuariosOnline(ambiente: ID!):[Usuario!]!
+            mensagensPorAmbiente(ambiente: ID!):[Mensagem!]!
+        }
+        type Mutation {     # metodos utilizados para fazer alteracoes insercoes e delecoes
+            registrarEntrada(usuario: ID!, ambiente: ID!): Participacao!
+            registrarSaida(usuario: ID!, ambiente: ID!): Participacao!
+            registrarMensagem(texto: String, usuario: ID!, ambiente: ID!): Mensagem!
+        }
+        type Subscription {
+            novaMensagem(ambiente: ID!): Mensagem!
+            usuarioEntrou(ambiente: ID!): Usuario!
+            usuarioSaiu(ambiente: ID!): Usuario!
         }
     `,
     // define os resolvers para o que foi falado que existe na interface
@@ -61,7 +75,90 @@ const schema = createSchema({
                     (u) => u.nome === nome && u.senha === senha
                 );
             },
-            usuarios: () => usuarios,
+            usuariosOnline: (parent, args, context, info) => {
+                const idUsuarios = participacoes
+                    .filter(
+                        (p) =>
+                            p.ambiente === args.ambiente && p.dataSaida === null
+                    )
+                    .map((p) => p.usuario);
+                return usuarios.filter((u) => idUsuarios.includes(u.id));
+            },
+            mensagensPorAmbiente: (parent, args, context, info) => {
+                return mensagens.filter((m) => m.ambiente === args.ambiente);
+            },
+        },
+        Mutation: {
+            registrarEntrada: (parent, args, context, info) => {
+                const { pubSub } = context;
+                const { usuario, ambiente } = args;
+                const participacao = {
+                    id: Math.random().toString(36).substring(2, 9),
+                    usuario,
+                    ambiente,
+                    dataEntrada: new Date().toISOString(),
+                    dataSaida: null,
+                };
+                participacoes.push(participacao);
+                pubSub.publish(`A${ambiente}: Novo Usuário`, {
+                    usuarioEntrou: usuarios.find((u) => u.id === usuario),
+                });
+                return participacao;
+            },
+            registrarSaida: (parent, args, context, info) => {
+                const { pubSub } = context;
+                const { usuario, ambiente } = args;
+                const p = participacoes.find(
+                    (p) =>
+                        p.usuario === usuario &&
+                        p.ambiente === ambiente &&
+                        p.dataSaida === null
+                ); // devolve uma variavel de referencia do objeto
+                p.dataSaida = new Date().toISOString();
+                pubSub.publish(`A${args.ambiente}: Usuario Saiu`, {
+                    usuarioSaiu: usuarios.find((u) => u.id === usuario),
+                });
+                return p;
+            },
+            registrarMensagem: (parent, args, contenxt, info) => {
+                const { pubSub } = contenxt;
+                const { texto, usuario, ambiente } = args;
+                const mensagem = {
+                    id: Math.random().toString(36).substring(2, 9),
+                    texto,
+                    usuario,
+                    ambiente,
+                    data: new Date().toISOString(),
+                };
+                mensagens.push(mensagem);
+                pubSub.publish(`A${args.ambiente}: Nova Mensagem`, {
+                    novaMensagem: mensagem,
+                });
+                return mensagem;
+            },
+        },
+        Subscription: {
+            novaMensagem: {
+                subscribe: (parent, args, context, info) => {
+                    return context.pubSub.subscribe(
+                        `A${args.ambiente}: Nova Mensagem`
+                    );
+                },
+            },
+            usuarioEntrou: {
+                subscribe: (parent, args, context, info) => {
+                    return context.pubSub.subscribe(
+                        `A${args.ambiente}: Novo Usuário`
+                    );
+                },
+            },
+            usuarioSaiu: {
+                subscribe: (parent, args, context, info) => {
+                    return context.pubSub.subscribe(
+                        `A${args.ambiente}: Usuario Saiu`
+                    );
+                },
+            },
         },
         Ambiente: {
             // Parent: referencia o ambiente da vez,
@@ -97,6 +194,9 @@ const schema = createSchema({
 
 const yoga = createYoga({
     schema,
+    context: {
+        pubSub,
+    },
 });
 
 const server = createServer(yoga);
